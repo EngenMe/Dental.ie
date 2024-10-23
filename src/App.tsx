14dbc7514c3bee2fd22e6113aa6e910e80433158
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-10-23 10:56:05 +0100

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import NavBar from './components/NavBar';
+import Pricing from './components/Pricing';
+import Testimonial from './components/Testimonial';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Pricing />
+      <About />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
