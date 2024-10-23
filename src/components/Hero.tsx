6bfed36b3e6b5a237b177c01dd67fee738562a51
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-10-23 20:44:05 +0100

    Build hero section.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,40 +1,30 @@
-import { ArrowDownRight } from 'lucide-react';
-
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import heroPicSm from '@/assets/heroPic-sm.webp';
+import heroPicMd from '@/assets/heroPic-md.webp';
+import heroPiclg from '@/assets/heroPic-lg.webp';
 
 const Hero1 = () => {
   return (
-    <section className="py-32">
-      <div className="container">
-        <div className="grid items-center gap-8 lg:grid-cols-2">
-          <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
-            <Badge variant="outline">
-              New Release
-              <ArrowDownRight className="ml-2 size-4" />
-            </Badge>
-            <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
-              Welcome to Our Website
-            </h1>
-            <p className="mb-8 max-w-xl text-muted-foreground lg:text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Elig
-              doloremque mollitia fugiat omnis! Porro facilis quo animi
-              consequatur. Explicabo.
-            </p>
-            <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-              <Button className="w-full sm:w-auto">Primary Button</Button>
-              <Button variant="outline" className="w-full sm:w-auto">
-                Secondary Button
-                <ArrowDownRight className="ml-2 size-4" />
-              </Button>
-            </div>
+    <section className="px-5 pt-20">
+      <div className="grid items-center gap-8 lg:grid-cols-2">
+        <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+          <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
+            Your Smile, Our Priority
+          </h1>
+          <p className="mb-8 max-w-xl text-muted-foreground lg:text-xl">
+            Comprehensive dental care for a healthier, brighter smile. Book your
+            appointment today and experience expert care in a warm,
+            family-friendly environment.
+          </p>
+          <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
+            <Button className="w-full sm:w-auto">Book Appointment</Button>
           </div>
-          <img
-            src="https://www.shadcnblocks.com/images/block/placeholder-1.svg"
-            alt="placeholder hero"
-            className="max-h-96 w-full rounded-md object-cover"
-          />
         </div>
+        <img
+          srcSet={`${heroPicSm} 640w, ${heroPicMd} 768w, ${heroPiclg} 1280w`}
+          alt="Dentist holding a beige mouth model and a dental tool."
+          className="max-h-96 w-full rounded-md object-cover"
+        />
       </div>
     </section>
   );
