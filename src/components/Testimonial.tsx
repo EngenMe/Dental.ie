857b915404b15210e5bcae7e1c2f3b92c6323051
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-10-24 20:14:21 +0100

    Build contact us section.

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -61,52 +61,50 @@ const testimonials = [
 const Testimonial = () => {
   return (
     <section className="py-20 px-5">
-      <div className="">
-        <Carousel className="w-full">
-          <div className="mb-8 flex justify-between md:px-10 px-2 lg:mb-12">
-            <h2 className="text-2xl font-semibold lg:text-5xl hidden md:block">
-              Why Clients Love Us
-            </h2>
-            <h2 className="text-2xl font-semibold lg:text-5xl md:hidden">
-              Why Clients <br /> Love Us
-            </h2>
-            <div className="flex items-center space-x-2">
-              <CarouselPrevious className="static translate-y-0" />
-              <CarouselNext className="static translate-y-0" />
-            </div>
+      <Carousel className="w-full">
+        <div className="mb-8 flex justify-between md:px-10 lg:px-20 px-2 lg:mb-12">
+          <h2 className="text-2xl font-semibold lg:text-5xl hidden md:block">
+            Why Clients Love Us
+          </h2>
+          <h2 className="text-2xl font-semibold lg:text-5xl md:hidden">
+            Why Clients <br /> Love Us
+          </h2>
+          <div className="flex items-center space-x-2">
+            <CarouselPrevious className="static translate-y-0" />
+            <CarouselNext className="static translate-y-0" />
           </div>
-          <CarouselContent>
-            {testimonials.map((testimonial, idx) => (
-              <CarouselItem
-                key={idx}
-                className="basis-full md:basis-1/2 lg:basis-1/3"
-              >
-                <div className="h-full p-1">
-                  <div className="flex h-full flex-col justify-between rounded-lg border p-6">
-                    <q className="leading-7 text-foreground/70">
-                      {testimonial.content}
-                    </q>
-                    <div className="mt-6 flex gap-4 leading-5">
-                      <Avatar className="size-9 rounded-full ring-1 ring-input">
-                        <AvatarImage
-                          src={testimonial.avatar}
-                          alt={testimonial.name}
-                        />
-                      </Avatar>
-                      <div className="text-sm">
-                        <p className="font-medium">{testimonial.name}</p>
-                        <p className="text-muted-foreground">
-                          {testimonial.role}
-                        </p>
-                      </div>
+        </div>
+        <CarouselContent>
+          {testimonials.map((testimonial, idx) => (
+            <CarouselItem
+              key={idx}
+              className="basis-full md:basis-1/2 lg:basis-1/3"
+            >
+              <div className="h-full p-1">
+                <div className="flex h-full flex-col justify-between rounded-lg border p-6">
+                  <q className="leading-7 text-foreground/70">
+                    {testimonial.content}
+                  </q>
+                  <div className="mt-6 flex gap-4 leading-5">
+                    <Avatar className="size-9 rounded-full ring-1 ring-input">
+                      <AvatarImage
+                        src={testimonial.avatar}
+                        alt={testimonial.name}
+                      />
+                    </Avatar>
+                    <div className="text-sm">
+                      <p className="font-medium">{testimonial.name}</p>
+                      <p className="text-muted-foreground">
+                        {testimonial.role}
+                      </p>
                     </div>
                   </div>
                 </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 };
