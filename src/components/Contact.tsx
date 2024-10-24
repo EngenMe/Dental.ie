1eaaf360bbd625bd7cc53b0fd505a9a907a2f43e
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-10-24 21:20:24 +0100

    Fix contact us card not clearly visible.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -35,26 +35,26 @@ const Contact2 = () => {
             </ul>
           </div>
         </div>
-        <div className="mx-auto flex max-w-screen-md flex-col gap-6 rounded-lg border p-10">
+        <div className="mx-auto bg-card-foreground text-card flex max-w-screen-md flex-col gap-6 rounded-lg border p-10">
           <div className="flex gap-4">
-            <div className="grid w-full items-center gap-1.5">
+            <div className="grid w-full items-center gap-3.5">
               <Label htmlFor="firstname">First Name</Label>
               <Input type="text" id="firstname" placeholder="First Name" />
             </div>
-            <div className="grid w-full items-center gap-1.5">
+            <div className="grid w-full items-center gap-3.5">
               <Label htmlFor="lastname">Last Name</Label>
               <Input type="text" id="lastname" placeholder="Last Name" />
             </div>
           </div>
-          <div className="grid w-full items-center gap-1.5">
+          <div className="grid w-full items-center gap-3.5">
             <Label htmlFor="email">Email</Label>
             <Input type="email" id="email" placeholder="Email" />
           </div>
-          <div className="grid w-full items-center gap-1.5">
+          <div className="grid w-full items-center gap-3.5">
             <Label htmlFor="subject">Subject</Label>
             <Input type="text" id="subject" placeholder="Subject" />
           </div>
-          <div className="grid w-full gap-1.5">
+          <div className="grid w-full gap-3.5">
             <Label htmlFor="message">Message</Label>
             <Textarea placeholder="Type your message here." id="message" />
           </div>
